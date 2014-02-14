4a9afc11f5bebfb276b1d530172a0c4d0242c764
greenplumguru <jgronline@gmail.com>
2014-02-14 14:43:36 -0600

    build script

---
+++ compile.sh
@@ -0,0 +1,8 @@
+javac *.java
+jar cfm Outsourcer.jar manifest.txt Logger.class CommonDB.class Oracle.class SQLServer.class ExternalData.class GP.class ExternalDataD.class ExternalDataThread.class
+jar cfm OutsourcerUI.jar manifest.txt *Model.class *View.class *Control.class UI.class ServerRunnerUI.class
+jar cfm OutsourcerScheduler.jar manifest.txt AgentD.class
+
+mv Outsourcer.jar jar/
+mv OutsourcerUI.jar jar/
+mv OutsourcerScheduler.jar jar/
